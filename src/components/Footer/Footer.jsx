d4dba4943fe818a884272df9c2d8e8377362409b
SpyBeast07 <kushagra7503@gmail.com>
2025-06-13 22:15:47 +0530

    Home page completed perfectly

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/images/youtube_icon.svg'
+import twitter_icon from '../../assets/images/twitter_icon.svg'
+import instagram_icon from '../../assets/images/instagram_icon.svg'
+import facebook_icon from '../../assets/images/facebook_icon.svg'
 
 const Footer = () => {
     return (
         <div className='footer'>
+            <div className="footer-icons">
+                <img src={youtube_icon} alt="Youtube" />
+                <img src={twitter_icon} alt="Twitter" />
+                <img src={instagram_icon} alt="Instagram" />
+                <img src={facebook_icon} alt="Facebook" />
+            </div>
+            
+            <div className="question">
+                Questions? Call <span>000-800-919-1743</span>
+            </div>
+            <div className="footer-menu">
+                <div className="footer-item">
+                    <a href="">FAQ</a>
+                    <a href="">Investor Relations</a>
+                    <a href="">Privacy</a>
+                    <a href="">Speed Test</a>
+                </div>
+                <div className="footer-item">
+                    <a href="">Help Centre</a>
+                    <a href="">Jobs</a>
+                    <a href="">Cookie Preferences</a>
+                    <a href="">Legal Notices</a>
+                </div>
+                <div className="footer-item">
+                    <a href="">Accounts</a>
+                    <a href="">Ways to Watch</a>
+                    <a href="">Corporate Information</a>
+                    <a href="">Only on Netflix</a>
+                </div>
+                <div className="footer-item">
+                    <a href="">Media Centre</a>
+                    <a href="">Terms of Use</a>
+                    <a href="">Contact Us</a>
+                    {/* <a href="">Only on Netflix</a> */}
+                </div>
+            </div>
 
+            <p className='name-tag'>
+                Netflix India
+            </p>
+
+            <p className='copyright-text'>
+                © 1997-2024 Netflix, Inc.
+            </p>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/images/hero_title.png'
 import play_icon from '../../assets/images/play_icon.svg'
 import info_icon from '../../assets/images/info_icon.svg'
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
     return (
@@ -13,28 +14,35 @@ const Home = () => {
             <Navbar />
             <div className="hero">
                 <img src={hero_banner} alt="Banner" className='banner-img' />
+
                 <div className="hero-caption">
                     <img src={hero_title} alt="Title" className='caption-img' />
                     <p>Rana Naidu is the go-to problem solver for the rich and famous. But when his father is released from jail, the one mess he can't handle may be his own.</p>
+
                     <div className="hero-btns">
                         <button className='btn'>
                             <img src={play_icon} alt="Play Button" />
                             Play
                         </button>
+
                         <button className='btn dark-btn'>
                             <img src={info_icon} alt="Info Button" />
                             More Info
                         </button>
                     </div>
-                    <TitleCards/>
                 </div>
             </div>
+
             <div className="more-cards">
-                <TitleCards/>
-                <TitleCards/>
-                <TitleCards/>
-                <TitleCards/>
+                <TitleCards />
+                <TitleCards title={"Horror Movie"} />
+                <TitleCards title={"Trending Now"} />
+                <TitleCards title={"Comedies"} />
+                <TitleCards title={"Action"} />
             </div>
+
+            <Footer />
+
         </div>
     )
 }
